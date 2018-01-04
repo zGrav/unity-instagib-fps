3785544d5fbd9bd6adba46cecf6021e28f5d7ca7
David Silva <zgrav@null.net>
2018-01-04 18:16:16 +0000

    feat(networking): add networking/player sync

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+	[SerializeField]
+	Behaviour[] disableComponents;
+
+	Camera sceneCamera;
+
+	void Start() {
+		if (!isLocalPlayer) {
+			foreach (Behaviour b in disableComponents) {
+				b.enabled = false;
+			}
+		} else {
+			sceneCamera = Camera.main;
+			if (sceneCamera != null) {
+				sceneCamera.gameObject.SetActive (false);
+			}
+		}
+	}
+
+	void onDisable() {
+		if (sceneCamera != null) {
+			sceneCamera.gameObject.SetActive (true);
+		}
+	}
+}
