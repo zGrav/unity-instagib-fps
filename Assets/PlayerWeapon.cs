c28fdb2b2c1347dcb5e4d68e6feed13a1a658100
David Silva <zgrav@null.net>
2018-01-04 19:33:03 +0000

    fix(PlayerWeapon): whoopsy damage

--- Assets/PlayerWeapon.cs
+++ Assets/PlayerWeapon.cs
@@ -6,6 +6,6 @@ public class PlayerWeapon: MonoBehaviour {
 	[SerializeField]
 	public string weaponName = "UBER-OMEGA-LEET-MLG-RAYGUN";
 
-	public float weaponDamage = 10f;
+	public float weaponDamage = 100f;
 	public float weaponRange = 300f;
 }
